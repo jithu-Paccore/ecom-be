306ed03fbab82b63d29f0cc3f6142ff169dd41ec
jithu-Paccore <jithendra.c@paccore.com>
2022-09-15 16:57:13 +0530

    filter products api added

--- routes/products.js
+++ routes/products.js
@@ -17,6 +17,41 @@ router.get("/getProduct", requireLogin, async (req, res) => {
   return res.send(productList);
 });
 
+router.get("/getFilterProduct", requireLogin, async (req, res) => {
+  let query = [];
+  let gender = req.body.gender;
+  let categoryCode = req.body.categoryCode;
+  let size = req.body.size;
+  let brand = req.body.brand;
+  if (gender.length > 0) {
+    query.push({ forGender: [gender, "MF"] });
+  }
+  if (categoryCode.length > 0) {
+    query.push({ categoryCode: categoryCode });
+  }
+  if (size.length > 0) {
+    query.push({ size: size });
+  }
+  if (brand.length > 0) {
+    query.push({ brand: brand });
+  }
+  console.log("query", query);
+  let prod = Product;
+  let productList = [];
+  if (prod) {
+    productList = await Product.find({
+      $and: query,
+    });
+  }
+
+  if (!productList) {
+    m;
+    return res.status(500).send({ success: false });
+  }
+  return res.send(productList);
+  console.log(productList.length);
+});
+
 router.post("/addProduct", async (req, res) => {
   let product = new Product({
     name: req.body.name,
